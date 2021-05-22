1df0b86110f3522252aa0b7c1bdcf47ca0471409
mayone-du <70831966+mayone-du@users.noreply.github.com>
2021-05-22 11:38:27 +0900

    Initialize develop

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "src/components/Layout";
+
+const Index: React.VFC = () => {
+  return (
+    <Layout>
+      <div className="flex flex-col min-h-screen justify-center items-center">
+        <div className="text-5xl">Index Page</div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Index;
